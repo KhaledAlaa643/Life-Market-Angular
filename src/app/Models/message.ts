4505e55f6c2b51fdedb479edf9a394e247e27055
walid <walidmostafa209@gmail.com>
2023-03-17 23:09:50 +0200

    modify contact us page and add api to it

---
+++ src/app/Models/message.ts
@@ -0,0 +1,7 @@
+export interface Message {
+    id?:string,
+    user_name:string,
+    email:string,
+    comment:string
+
+}
---
+++ src/app/Services/message.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Message } from '../Models/message';
+import { Observable, retry } from 'rxjs';
+import { environment } from 'src/environments/environment.development';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MessageService {
+
+  constructor(private _httpClient:HttpClient) { }
+
+  saveNewMessage(newMess:Message):Observable<Message>{
+    return this._httpClient.post<Message>(
+      `${environment.apiURL}/contact_us/`,
+      newMess,
+      {
+        headers:new HttpHeaders({
+          'accept':"application/json"
+        })
+      }
+    ).pipe(retry(2));
+  }
+}
