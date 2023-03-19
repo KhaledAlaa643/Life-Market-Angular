a60418e51f1b8c3a01c9ee32f6108c1b73e4a364
walid <walidmostafa209@gmail.com>
2023-03-19 14:46:59 +0200

    add top selling and top rating product to profile page

--- src/app/component/profile/profile.component.ts
+++ src/app/component/profile/profile.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Product } from 'src/app/Models/product';
+import { ProductsService } from 'src/app/Services/products.service';
 import { UserService } from 'src/app/services/user.service';
 import { User } from 'src/app/viewmodules/user';
 
@@ -8,10 +10,58 @@ import { User } from 'src/app/viewmodules/user';
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit{
+
   user: User = {} as User;
   res!: any;
-  constructor(private userserve: UserService, private route: Router) { }
+  topSellingProducts: Product[] = [];
+  topSelling: Product[] = [];
+  topRatingProducts: Product[] = [];
+  topRating: Product[] = [];
+  
+  constructor(
+    private userserve: UserService,
+    private route: Router,
+    private _productServ: ProductsService,
+    private router:Router,
+  ){}
+
+
+  ngOnInit(): void {
+    this._productServ.getTopSellingProducts().subscribe({
+      next: (res) => {
+        this.topSellingProducts = res;
+        for(let i=0;i<6;i++){
+          this.topSelling[i] = this.topSellingProducts[i];
+        }
+      }
+    });
+    this._productServ.getTopRatingProducts().subscribe({
+      next: (res) => {
+        this.topRatingProducts = res;
+        for(let i=0;i<6;i++){
+          this.topRating[i] = this.topRatingProducts[i];
+        }
+        // console.log(this.topSelling);
+      }
+    });
+  }
+
+
+  goToPrdList(_type:any, id:any){
+    if(_type == "cat"){
+      this.router.navigate(['main/products/list/category', id], { queryParams: { type: _type} });
+    }
+    else{
+      this.router.navigate(['main/products/list', id], { queryParams: { type: _type} });
+    }
+  }
+
+
+  goToPrdDetails(id:any){
+    this.router.navigate(['main/product/', id]);
+  }
+
 
   logoutFun() {
     return this.userserve.logout().subscribe({
