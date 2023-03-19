e69320b5d4362c796c45b683d452d67c9cd99eaa
Naglaa99 <Naglaahamdi6@gmail.com>
2023-03-19 05:03:57 +0200

    create address service and connect with api

--- src/app/Services/address.service.ts
+++ src/app/Services/address.service.ts
@@ -6,31 +6,31 @@ import { Address } from '../viewmodules/address';
   providedIn: 'root'
 })
 export class AddressService {
-constructor(private httpservice: HttpClient) { }
-address:Address[]=[]
-getaddressData(): Observable<Address> {
-  return this.httpservice.get<Address>('http://localhost:8000/api/address', {
-    headers: new HttpHeaders({
-      accept: 'application/json'
-    })
-  });
-}
-CheckAddress(){
-  return localStorage.getItem("userId")
-   
- }
- UpdateAdress(addressData:Address): Observable<Address> {
-  return this.httpservice.post<Address>('http://localhost:8000/api/address/update',addressData, {
-    headers: new HttpHeaders({
-      accept: 'application/json'
-    })
-  });
-}
-CreateAdress(addressData:Address): Observable<Address> {
-  return this.httpservice.post<Address>('http://localhost:8000/api/address/create',addressData, {
-    headers: new HttpHeaders({
-      accept: 'application/json'
-    })
-  });
-}
+  constructor(private httpservice: HttpClient) { }
+  address: Address[] = []
+  getaddressData(): Observable<Address> {
+    return this.httpservice.get<Address>('http://localhost:8000/api/address', {
+      headers: new HttpHeaders({
+        accept: 'application/json'
+      })
+    });
+  }
+  CheckAddress() {
+    return localStorage.getItem("userId")
+
+  }
+  UpdateAdress(addressData: Address): Observable<Address> {
+    return this.httpservice.post<Address>('http://localhost:8000/api/address/update', addressData, {
+      headers: new HttpHeaders({
+        accept: 'application/json'
+      })
+    });
+  }
+  CreateAdress(addressData: Address): Observable<Address> {
+    return this.httpservice.post<Address>('http://localhost:8000/api/address/create', addressData, {
+      headers: new HttpHeaders({
+        accept: 'application/json'
+      })
+    });
+  }
 }
