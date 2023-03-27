0f7a67195c5d9386c72c22a8520007fed546af7f
Naglaa99 <Naglaahamdi6@gmail.com>
2023-03-27 13:38:00 +0200

    make filter by order status

--- src/app/viewmodules/order.ts
+++ src/app/viewmodules/order.ts
@@ -1,17 +1,17 @@
 export interface Order {
-        products: any;
-        id: number;
-        created_at: string;
-        updated_at: string;
-        user_id: number;
-        status?: string;
-        total?: number;
-        delivery_price_id?: number;
-        first_name?:string;
-        last_name?:string;
-        email?:string;
-        index?:number;
-        delivery_price?:number;
-      }
+  products: any;
+  id: number;
+  created_at: string;
+  updated_at: string;
+  user_id: number;
+  status?: string;
+  total?: number;
+  delivery_price_id?: number;
+  first_name?: string;
+  last_name?: string;
+  email?: string;
+  index?: number;
+  delivery_price?: number;
+}
       
 
