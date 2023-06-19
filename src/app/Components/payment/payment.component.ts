d7b9e66ccd460121b0d20df3cd95869d067b85ce
KhaledAlaa643 <122280211+KhaledAlaa643@users.noreply.github.com>
2023-06-19 19:03:43 +0300

    add

--- src/app/Components/payment/payment.component.ts
+++ src/app/Components/payment/payment.component.ts
@@ -1,46 +1,42 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-import { CartService } from 'src/app/Services/cart.service';
+import { HttpClient } from '@angular/common/http'
+import { Component, OnInit } from '@angular/core'
+import { CartService } from 'src/app/Services/cart.service'
 import { render } from 'creditcardpayments/creditCardPayments'
-import { Payment } from 'src/app/Models/payment';
-import { CreateOrderService } from 'src/app/Services/create-order.service';
-import Swal from 'sweetalert2';
-import { Router } from '@angular/router';
+import { Payment } from 'src/app/Models/payment'
+import { CreateOrderService } from 'src/app/Services/create-order.service'
+import Swal from 'sweetalert2'
+import { Router } from '@angular/router'
 @Component({
   selector: 'app-payment',
   templateUrl: './payment.component.html',
-  styleUrls: ['./payment.component.css']
+  styleUrls: ['./payment.component.css'],
 })
-
 export class PaymentComponent implements OnInit {
-
   payment: Payment = {} as Payment
-  totalPrice!: any;
+  totalPrice!: any
 
   constructor(
     private cartService: CartService,
     private createOrder: CreateOrderService,
     private router: Router,
-  ) { }
+  ) {}
 
   ngOnInit(): void {
-
-    const storedTotalPrice = localStorage.getItem('totalPrice');
+    const storedTotalPrice = localStorage.getItem('totalPrice')
 
     if (storedTotalPrice) {
-      this.totalPrice = parseFloat(storedTotalPrice);
-    }
-    else {
-      this.totalPrice = 0;
+      this.totalPrice = parseFloat(storedTotalPrice)
+    } else {
+      this.totalPrice = 0
     }
 
-    this.cartService.totalPrice.subscribe(totalPrice => {
+    this.cartService.totalPrice.subscribe((totalPrice) => {
       this.totalPrice = storedTotalPrice
-    });
+    })
   }
 
   pay() {
-    this.payment.amount = Math.floor((this.totalPrice / 30) * 100);
+    this.payment.amount = Math.floor((this.totalPrice / 30) * 100)
     // console.log(this.payment);
     this.createOrder.orderData(this.payment).subscribe({
       next: (res) => {
@@ -48,23 +44,19 @@ export class PaymentComponent implements OnInit {
           icon: 'success',
           text: 'Your Order is Success!',
           showConfirmButton: false,
-          timer: 1500
+          timer: 1500,
         }).then(() => {
-          localStorage.removeItem('totalPrice');
+          localStorage.removeItem('totalPrice')
           this.router.navigate(['/main/home'])
-        });
-
+        })
       },
       error: (err) => {
         Swal.fire({
           icon: 'error',
           title: 'The card data is incorrect, please verify the entered data',
         })
-        console.log(err);
-
-      }
-    });
-
+        console.log(err)
+      },
+    })
   }
 }
-
